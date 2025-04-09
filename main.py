4716dfa42d5b278bf5a451c5bf8282196a69b67c
Aaryan <downloadsoft17@gmail.com>
2025-04-09 08:06:16 +0545

    Populate the SQL table with data.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QTableWidget
-
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QTableWidget, QTableWidgetItem
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -23,14 +23,22 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Phone Number"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
-
     def load_data(self):
-        pass
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
 
+        connection.close()
 
 app = QApplication(sys.argv)
 mainWindow = MainWindow()
 mainWindow.show()
+mainWindow.load_data()
 sys.exit(app.exec())
